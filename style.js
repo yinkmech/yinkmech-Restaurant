13ddcd1c70c1156f93292f8e00783a1daf4e0b9b
yinkmech <80957517+yinkmech@users.noreply.github.com>
2021-03-29 02:51:07 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,18 @@
+
+
+function getStarted() {
+  
+  alert('Hello Customer! 😊');
+  
+var CustomerInterest = confirm('Would you like a taste of our classic dishes');
+
+if (CustomerInterest == true) {
+       alert('We serve jollof rice at $1000 per plate'); 
+       
+} else {
+       alert('okay, let me get the menu for you'); 
+}
+
+}
+
+  
